e9c5b087eb94f4d7e580a92ed855df9e1f49eed0
ssemlitz <sdsemlitz@yahoo.com>
2022-07-26 15:03:45 -0400

    refactor profile controller functions

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,9 +1,8 @@
 import { Profile } from '../models/profile.js'
-import { v2 as cloudinary } from 'cloudinary'
+import {AffirmationRef} from '../models/affirmation.js'
 
 function index(req, res) {
   Profile.find({})
-  .populate('affirmations')
   .then(profiles => res.json(profiles))
   .catch(err => {
     console.log(err)
@@ -18,7 +17,7 @@ function create(req, res) {
     profile.affirmations.push(req.body)
     profile.save()
     .then(() => {
-      res.json(affirmation)
+      res.json(profile)
     })
   })
   .catch(err => {
@@ -27,9 +26,23 @@ function create(req, res) {
   })
 }
 
+function deleteOne(req, res) {
+	Profile.findById(req.user.profile)
+		.then(profile => {
+		profile.affirmations.remove({ _id: req.params.affirmationId })
+		profile.save()
+			.then(profile => {
+				res.json(profile)
+			})
+		})
+		.catch(err => {
+      console.log(err)
+      res.status(500).json({err: err.errmsg})
+    })
+	}
+
 function show(req, res) {
   Profile.findById(req.params.id)
-  .populate('affirmations')
   .then(profile => {
     res.json(profile)
   })
@@ -39,28 +52,9 @@ function show(req, res) {
   })
 }
 
-function addPhoto(req, res) {
-  const imageFile = req.files.photo.path
-  Profile.findById(req.params.id)
-  .then(profile => {
-    cloudinary.uploader.upload(imageFile, {tags: `${profile.email}`})
-    .then(image => {
-      profile.photo = image.url
-      profile.save()
-      .then(profile => {
-        res.status(201).json(profile.photo)
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json(err)
-    })
-  })
-}
-
 export { 
   index, 
   create,
-  addPhoto, 
+  deleteOne as delete ,
   show
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -9,9 +9,10 @@ const router = Router()
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, profilesCtrl.create)
+router.delete('/affirmation/:affirmationId', checkAuth, profilesCtrl.delete)
+router.post('/:id/affirmations', checkAuth, profilesCtrl.create)
 router.get('/', checkAuth, profilesCtrl.index)
-router.get('/id', checkAuth, profilesCtrl.show)
-router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+router.get('/:id', checkAuth, profilesCtrl.show)
+// router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 
 export { router }
